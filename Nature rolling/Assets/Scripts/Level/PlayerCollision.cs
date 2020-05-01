dda839a47e19e61e9132f5185500be3689f1905a
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-02 00:56:42 +0200

    Made an end point

--- Nature rolling/Assets/Scripts/Level/PlayerCollision.cs
+++ Nature rolling/Assets/Scripts/Level/PlayerCollision.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public PlayerMovement movement;
+    public TextMeshProUGUI text;
+    public MeshCollider gateCollider;
+    public MeshRenderer gateRender;
     void OnCollisionEnter(Collision collisionInfo)
     {
-        if (collisionInfo.collider.tag == "Gate") 
+        if (collisionInfo.collider.tag == "Key")
         {
-            Debug.Log("Hit!");
+            text.text = "Task complete!\nNow go through the opened path!";
+            gateCollider.enabled = false;
+            gateRender.enabled = false;
+        }
+
+        if (collisionInfo.collider.tag == "End") 
+        {
+            movement.enabled = false;
         }
     }
 
