a61f54c6e0340c5a2460803c2100ea1e0e1cb63e
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-01 20:22:51 +0200

    Introduced player movement

---
+++ Nature rolling/Assets/Scripts/Level/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force = 200f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w")) 
+        {
+            rb.AddForce(0, 0, force * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-force * Time.fixedDeltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -force * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(force * Time.fixedDeltaTime, 0, 0);
+        }
+    }
+}
