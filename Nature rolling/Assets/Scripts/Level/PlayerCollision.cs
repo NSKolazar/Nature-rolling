c821de654250061640ab34850953851ef061019e
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-02 02:09:30 +0200

    Added audio

--- Nature rolling/Assets/Scripts/Level/PlayerCollision.cs
+++ Nature rolling/Assets/Scripts/Level/PlayerCollision.cs
@@ -1,25 +1,35 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public PlayerMovement movement;
     public TextMeshProUGUI text;
     public MeshCollider gateCollider;
     public MeshRenderer gateRender;
+    public AudioSource point;
+    public AudioSource complete;
     void OnCollisionEnter(Collision collisionInfo)
     {
-        if (collisionInfo.collider.tag == "Key")
+        if (collisionInfo.collider.CompareTag("Key"))
         {
             text.text = "Task complete!\nNow go through the opened path!";
+            point.Play();
             gateCollider.enabled = false;
             gateRender.enabled = false;
+            
         }
 
-        if (collisionInfo.collider.tag == "End") 
-        {
-            movement.enabled = false;
+        if (collisionInfo.collider.CompareTag("End"))
+        {       
+            text.text = "Level complete!";
+            complete.Play();
+            Invoke("EndGame",2f);           
         }
     }
 
+    void EndGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
